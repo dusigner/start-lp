985c45274f6ab2d9db12c0341e03d264e90ae4fd
Eduardo Soares <eduardo@visie.com.br>
2020-09-03 17:52:43 -0300

    terceiro test

--- react/lancamentoRefriVideo/layout/scrollvideo/scrollvideo.js
+++ react/lancamentoRefriVideo/layout/scrollvideo/scrollvideo.js
@@ -3,6 +3,7 @@ import ScrollMagic from "scrollmagic";
 //all the css animations need gsap as dependency
 import gsap, { TimelineMax, TweenMax, TweenLite } from "gsap";
 import './scrollvideo.global.css';
+import 'scrollmagic/scrollmagic/uncompressed/plugins/debug.addIndicators';
 
 class Scrollvideo extends React.Component {
   constructor(props) {
@@ -10,131 +11,120 @@ class Scrollvideo extends React.Component {
   }
 
     componentDidMount() {
+        
         const video = document.querySelector("video");
-        const scenes = document.querySelector(".scenes");
         const sceneIntro = document.querySelector(".scene1");
         const sceneDispe = document.querySelector(".scene2");
         const sceneFresh = document.querySelector(".scene3");
+        const sceneSpace = document.querySelector(".scene4");
         //END SECTION
         const section = document.querySelector("section");
-        const end = section.querySelector("h1");
 
         //SCROLLMAGIC
         const controller = new ScrollMagic.Controller({addIndicators: true});
 
-        //Scenes
+        //Pause Video
+        function pauseVideo() {
+            video.pause();
+        }
 
+        //Scenes
+        /** Intro */
         let scene1 = new ScrollMagic.Scene({
-            duration: 1,
+            duration: 1000,
             triggerElement: sceneIntro,
             triggerHook: 0
         })
-        .setPin(sceneIntro)
         .addTo(controller)
-        .trigger("change", scene1Anim)
-        .on("leave", playVideoScene1)
-        .addIndicators({name: "pin scene", colorEnd: "#FFFFFF"});
+        .setPin(sceneIntro)
+        .on("start", 
+            function () {
+                video.play();
+                // Exibe o Titulo
+                TweenMax.fromTo(sceneIntro, 1,  { opacity: 0 }, { opacity: 1}).delay(2)
+                video.addEventListener("timeupdate", function(){
+                    if(video.currentTime >= 2.7){
+                        video.pause()
+                    }
+                })
+            })
+        .on("update",
+            function () {
+                video.play()
+                // Oculta o Titulo
+                TweenMax.fromTo(sceneIntro, 1,  { opacity: 1 }, { opacity: 0})
+                // Exibe o Dispenser
+                TweenMax.fromTo(sceneDispe, 1,  { opacity: 0 }, { opacity: 1})
+                video.addEventListener("timeupdate", function(){
+                    if(video.currentTime >= 14.7){
+                        video.pause()
+                    }
+                })
+            })
 
-        function pauseVideoScene1() {
-            video.pause();
-        }
-        function playVideoScene1() {
-            video.play();
-        }
-        
-        const scene1Anim =  video.play() & TweenMax.fromTo(sceneIntro, 1,  { opacity: 0 }, { opacity: 1, onComplete:pauseVideoScene1}).delay(2)
-        const scene2Anim =  video.play() & console.log("TESTE")
-
+        /** Dispenser */
         let scene2 = new ScrollMagic.Scene({
-            duration: 1,
+            duration: 1000,
             triggerElement: sceneDispe,
             triggerHook: 0
         })
-        .setPin(sceneDispe)
         .addTo(controller)
-        .on("enter", pauseVideoScene2)
-        .addIndicators({name: "pin scene", colorEnd: "#FFFFFF"});
-
-        function pauseVideoScene2() {
-            video.play()
-            TweenMax.fromTo(sceneDispe, 1,  { opacity: 0 }, { opacity: 1, delay: 2, onComplete:pauseVideoScene1})
-            video.addEventListener("timeupdate", function(){
-               if(video.currentTime >= 5.0){
-                   video.pause()
-               }
+        .setPin(sceneDispe)
+        .on("end",
+            function () {
+                video.play()
+                // Oculta o Titulo
+                TweenMax.fromTo(sceneDispe, 1,  { opacity: 1 }, { opacity: 0})
+                // Exibe o Fresh
+                TweenMax.fromTo(sceneFresh, 1,  { opacity: 0 }, { opacity: 1})
+                video.addEventListener("timeupdate", function(){
+                    if(video.currentTime >= 20.10){
+                        video.pause()
+                    }
+                })
             })
-        }
 
+        /** Fresh */
         let scene3 = new ScrollMagic.Scene({
-            duration: 500,
+            duration: 1000,
             triggerElement: sceneFresh,
             triggerHook: 0
         })
-        .setPin(sceneFresh)
         .addTo(controller)
-        .addIndicators({name: "pin scene", colorEnd: "#FFFFFF"});
+        .setPin(sceneFresh)
+        .on("update",
+            function () {
+                video.play()
+                console.log("Play Video")
+                // Oculta o Titulo
+                TweenMax.fromTo(sceneFresh, 1,  { opacity: 1 }, { opacity: 0})
+                // Exibe o Fresh
+                TweenMax.fromTo(sceneSpace, 1,  { opacity: 0 }, { opacity: 1})
+                video.addEventListener("timeupdate", function(){
+                    if(video.currentTime >= 20.10){
+                        video.pause()
+                    }
+                })
+            })
+
+        /** Space */
+        let scene4 = new ScrollMagic.Scene({
+            duration: 1000,
+            triggerElement: sceneSpace,
+            triggerHook: 0
+        })
+        .addTo(controller)
+        .setPin(sceneSpace)
+        .on("end",
+            function () {
+                video.play()
+            })
 
 
+            video.addEventListener("timeupdate", function(){
+                console.log(video.currentTime)
+            })
 
-        // scene.on("progress", e => {
-        //     console.log("Scene progress changed to " + event.progress);
-        //     if(valid == 1){
-        //         video.play()
-        //         TweenMax.fromTo(sceneIntro, 1,  { opacity: 1 }, { opacity: 0})
-                
-        //     }
-        //     if(video.currentTime >= "02.0"){
-        //         if(valid == 2){
-        //             TweenMax.fromTo(sceneIntro, 1,  { opacity: 1 }, { opacity: 0}) & 
-        //             TweenMax.fromTo(sceneDispen, 1,  { opacity: 1 }, { opacity: 0})
-        //         }
-        //     }
-        //     if(video.currentTime >= "05.0"){
-        //         video.pause()
-        //     }
-            
-            
-            
-            
-        //     //TweenMax.fromTo(sceneDispen, 1,  { opacity: 0 }, { opacity: 1}).delay(2)
-        //     scrollpos = e.scrollPos / 1000;
-        // });
-
-        
-
-
-        //Scene1 Animation
-        // const scene1Anim =  video.play() & TweenMax.fromTo(sceneIntro, 1,  { opacity: 0 }, { opacity: 1, onComplete:pauseVideoScene1}).delay(2)
-        // const scene2Anim =  
-        //     video.play() & 
-        //     TweenMax.fromTo(sceneIntro, 1,  { opacity: 1 }, { opacity: 0}) & 
-        //     TweenMax.fromTo(sceneDispen, 1,  { opacity: 1 }, { opacity: 0})
-
-        //Video Animation
-        // let accelamount = 0.1;
-        // let scrollpos = 0;
-        // let delay = 0;
-
-        // scene.on("update", e => {
-        //     scrollpos = e.scrollPos / 1000;
-        // });
-
-        // setInterval(() => {
-        // delay += (scrollpos - delay) * accelamount;
-        // video.currentTime = delay;
-        // }, 33.3);
-
-        // if(window.scrollY == 0){
-        //     setTimeout(function(){ 
-        //         window.scrollBy({
-        //             top: 2200,
-        //             behavior: 'smooth'
-        //         }); 
-        //     }, 5000);
-        // }
-        // if(window.scrollY > 2222){
-        //     alert("oi")
-        // }
 
   }
 
@@ -149,14 +139,17 @@ class Scrollvideo extends React.Component {
         </div>
         <div className="scene2">
             <div className="scene2__content">
-                <h2>Brastemp Inverse | 44</h2>
-                <p>Máximo design, máxima sofisticação. Com 4 compartimentos e mais de 500L para você armazenar tudo o que quiser.</p>
+                <h2>Brastemp Dispenser</h2>
             </div>
         </div>
         <div className="scene3">
             <div className="scene3__content">
-                <h2>Brastemp Inverse | 444</h2>
-                <p>Máximo design, máxima sofisticação. Com 4 compartimentos e mais de 500L para você armazenar tudo o que quiser.</p>
+                <h2>Brastemp Fresh</h2>
+            </div>
+        </div>
+        <div className="scene4">
+            <div className="scene4__content">
+                <h2>Brastemp Space</h2>
             </div>
         </div>
         <video className="video" preload="true" controls muted>

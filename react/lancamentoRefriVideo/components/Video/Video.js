0ed16f40922de599b420d158fde2b03e3027a468
Eduardo Soares <eduardo@visie.com.br>
2020-09-09 11:29:56 -0300

    added section video

---
+++ react/lancamentoRefriVideo/components/Video/Video.js
@@ -0,0 +1,110 @@
+import React, { Component } from 'react'
+
+class Video extends React.Component {
+
+    componentDidMount() {
+        //window.addEventListener('scroll', this.onWindowScroll);
+        var video = document.getElementById("myVideo");
+
+        let firstPush = 0,
+            secondPush = 0,
+            thirdPush = 0,
+            fourthPush = 0;
+
+        video.addEventListener("timeupdate", function(){
+
+            const videoTime = (video.currentTime / video.duration * 100).toFixed(2);
+
+            if (firstPush==0) {
+                if (videoTime >= 25){
+                    dataLayer.push({
+                        event: 'generic',                     
+                        category: 'lavadora_edge',
+                        action: 'video_nova_geração',
+                        label: '25'
+                    })
+                    firstPush=1;
+                }
+            }
+            if (secondPush==0) {
+                if (videoTime >= 50){
+                    dataLayer.push({
+                        event: 'generic',                     
+                        category: 'lavadora_edge',
+                        action: 'video_nova_geração',
+                        label: '50'
+                    })
+                    secondPush=1;
+                }
+            }
+            if (thirdPush==0) {
+                if (videoTime >= 75){
+                    dataLayer.push({
+                        event: 'generic',                     
+                        category: 'lavadora_edge',
+                        action: 'video_nova_geração',
+                        label: '75'
+                    })
+                    thirdPush=1;
+                }
+            }
+            if (fourthPush==0) {
+                if (videoTime >= 100){
+                    dataLayer.push({
+                        event: 'generic',                     
+                        category: 'lavadora_edge',
+                        action: 'video_nova_geração',
+                        label: '100'
+                    })
+                    fourthPush=1;
+                }
+            }
+
+        })
+    }
+     
+    // componentWillUnmount() {
+    //     window.removeEventListener('scroll', this.onWindowScroll);
+    // }
+
+    // onWindowScroll = () => {
+
+    //     const vid = document.getElementById("myVideo");
+
+    //     function playVid() {
+    //         vid.play();
+    //     }
+
+    //     function pauseVid() {
+    //         vid.pause();
+    //     }
+
+    //     const observer = new IntersectionObserver(function(entries) {
+    //         if(entries[0].isIntersecting === true) {
+    //             vid.play();
+                
+    //         } else {
+    //             vid.pause();
+    //         }
+    //     }, { threshold: [0.5] });
+        
+    //     observer.observe(document.querySelector("#myVideo"));
+    // }
+
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <video id="myVideo" poster={this.props.poster} className="video" width="100%" height="100%" controls>
+                <svg width="186" height="186" viewBox="0 0 186 186" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path fill-rule="evenodd" clip-rule="evenodd" d="M77.1987 59.3511L122.677 93.8915L77.1987 128.432" stroke="#F08B1D" stroke-width="5" stroke-linecap="round" stroke-linejoin="round"/>
+                    <circle cx="93.3166" cy="93.3167" r="90.1834" stroke="#F08B1D" stroke-width="5"/>
+                </svg>
+                <source src={this.props.setSrc} type="video/mp4"></source>
+            </video>
+        )
+    }
+}
+export default Video;

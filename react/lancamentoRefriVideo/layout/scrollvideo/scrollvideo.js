cefc5cfde06d45a1f90e665f24b8c931c62d7c33
Eduardo Soares <eduardo@visie.com.br>
2020-09-10 10:20:43 -0300

    added convertible space

--- react/lancamentoRefriVideo/layout/scrollvideo/scrollvideo.js
+++ react/lancamentoRefriVideo/layout/scrollvideo/scrollvideo.js
@@ -9,9 +9,17 @@ import AtencaoDetalhes from '../../../lancamentoRefri/layout/atencao-detalhes/at
 import Video from '../video/video';
 
 class Scrollvideo extends React.Component {
-  constructor(props) {
-    super(props);
-  }
+    constructor(props) {
+		super(props);
+		this.state = {
+			value: 0
+		}
+	}
+    
+    handleChange = (event) => {
+        this.setState({value: event.target.value});
+        console.log(this.state.value);
+    }
 
     componentDidMount() {
         
@@ -65,7 +73,7 @@ class Scrollvideo extends React.Component {
             if(video.currentTime >= 2.2 && video.currentTime <= 2.7){
                 video.pause()
                 TweenLite.to(scene1, 1, {opacity: 1});
-                TweenLite.to(scene2, 1, {opacity: 0});
+                TweenLite.to([scene2,scene3,scene4], 1, {opacity: 0});
             }
             /** Scene 2 - PLAY */
             if(video.currentTime >= 2.8 && video.currentTime <= 3.7){
@@ -75,6 +83,7 @@ class Scrollvideo extends React.Component {
             /** Scene 2 */
             if(video.currentTime >= 6.0 && video.currentTime <= 6.7){
                 TweenLite.to(scene2, 1, {opacity: 1, zIndex:3,});
+                TweenLite.to([scene1,scene3,scene4], 1, {opacity: 0});
                 $("#gelo").addClass('active');
             }
             /** Scene 2 */
@@ -95,6 +104,7 @@ class Scrollvideo extends React.Component {
             if(video.currentTime >= 17.8 &&  video.currentTime <= 18.3){
                 TweenLite.to(scene3, 1, {opacity: 1, zIndex:3,});
                 video.pause()
+                TweenLite.to([scene1,scene2,scene4], 1, {opacity: 0});
             }
             /** Scene 4 - PLAY */
             if(video.currentTime >= 18.4 &&  video.currentTime <= 19.1){
@@ -104,6 +114,8 @@ class Scrollvideo extends React.Component {
             /** Scene 4 - PAUSE */
             if(video.currentTime >= 32){
                 video.pause()
+                TweenLite.to(scene4, 1, {opacity: 1, zIndex:3,});
+                TweenLite.to([scene1,scene2,scene3], 1, {opacity: 0});
             }
           });
 
@@ -162,7 +174,37 @@ class Scrollvideo extends React.Component {
             </div>
             <div className="scene4">
                 <div className="scene4__content">
-                    <h2>Brastemp Space</h2>
+                <div id="convertible-space" className={`convertible-space  ${this.state.value < 110 ? "vinhos" : ""} ${this.state.value > 110 && this.state.value < 260 ? "frutas-vegetais" : "" } ${this.state.value > 260 && this.state.value < 550 ? "carnes-peixes" : "" } ${this.state.value > 550 && this.state.value < 740 ? "congelamento-suave" : "" } ${this.state.value > 740 && this.state.value < 920 ? "congelamento-medio" : "" } ${this.state.value > 920 ? "congelamento-intenso" : "" } `}>
+
+<Title setClass="center" textMF="Convertible Space" textDF="Convertible Space"/>
+
+<p>Compartimento com controle independente <br className="mobile"></br> de <br className="desktop"></br> temperatura, você escolhe entre freezer ou <br className="mobile"></br><br className="desktop"></br> refrigerador com mais de 12 opções de <br className="mobile"></br><br className="desktop"></br> temperatura</p>
+
+<div className="line-chose">
+
+    <div className="line-text">
+        <h3>MODO<br></br>REFRIGERADOR</h3>
+        <p className={`${this.state.value < 110 ? "active" : "" }`}>10°C</p>
+        <p className={`${this.state.value > 110 && this.state.value < 260   ? "active" : "" }`}>4°C</p>
+        <p className={`${this.state.value > 260 && this.state.value < 550 ? "active" : "" }`}>0°C</p>
+        <h3>MODO<br></br>FREEZER</h3>
+        <p className={`${this.state.value > 550 && this.state.value < 740 ? "active" : "" }`}>-7°C</p>
+        <p className={`${this.state.value > 740 && this.state.value < 920 ? "active" : "" }`}>-12°C</p>
+        <p className={`${this.state.value > 920 ? "active" : "" }`}>-16°C a<br></br>22°C</p>
+    </div>
+
+    <input 
+    className="slider"
+    type="range" 
+    min="0" max="1000" 
+    value={this.state.value} 
+    onChange={this.handleChange}
+    data-orientation="vertical"
+    step="1"/>
+
+</div>
+
+</div>
                 </div>
             </div>
             <video className="videobg" preload="true" muted>

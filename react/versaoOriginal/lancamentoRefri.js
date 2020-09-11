5feccc30cb2858dc02fa31a6529887c77214fa0f
Wellington Moreira <wellington@visie.com.br>
2020-09-11 14:10:21 -0300

    ajuste de pagina do QRCode

--- react/versaoOriginal/lancamentoRefri.js
+++ react/versaoOriginal/lancamentoRefri.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Headroom from 'react-headroom';
 
 // Layout
+import Menu from './components/menu-estatico/menu'
 import GeladeiraInverse from './layout/geladeira-inverse/geladeira-inverse';
 import Dispenser from './layout/dispenser/dispenser';
 import FreshControl from './layout/fresh-control/fresh-control';
@@ -30,18 +31,20 @@ class LancamentoRefri extends React.Component {
 
 		return (
 			<div className="lancamento__refri">
+
 				<Headroom disableInlineStyles>
+					<Menu></Menu>
 				</Headroom>
 
-					<GeladeiraInverse></GeladeiraInverse>
-					<Dispenser></Dispenser>
-					<FreshControl></FreshControl>
-					<ConvertibleSpace></ConvertibleSpace>
-					<AtencaoDetalhes></AtencaoDetalhes>
-					<Video></Video>
-					{/* <Compra></Compra> */}
-					<Vitrine></Vitrine>
-					<EspecificacoesTecnicas></EspecificacoesTecnicas>
+				<GeladeiraInverse></GeladeiraInverse>
+				<Dispenser></Dispenser>
+				<FreshControl></FreshControl>
+				<ConvertibleSpace></ConvertibleSpace>
+				<AtencaoDetalhes></AtencaoDetalhes>
+				<Video></Video>
+				{/* <Compra></Compra> */}
+				<Vitrine></Vitrine>
+				<EspecificacoesTecnicas></EspecificacoesTecnicas>
 
 			</div>
 		)

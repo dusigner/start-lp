44094d374ce08f85b09ceb7a5d22230c10c11b10
Eduardo Soares <eduardo@visie.com.br>
2020-09-14 10:02:29 -0300

    change img poster on video

--- react/versaoOriginal/layout/video/video.js
+++ react/versaoOriginal/layout/video/video.js
@@ -10,7 +10,7 @@ class GeladeiraInverse extends React.Component {
             <div id="video-refri" className="video-refri">
 
                 <Video 
-                    poster="https://img.imageboss.me/cdn/http://bimg.visie.com.br/media/promo-video-thumb-brastemp.jpg" 
+                    poster="https://img.imageboss.me/cdn/http://bimg.visie.com.br/media/video-promo-video-thumb-brastemp-1.jpg" 
                     setSrc="http://consulwp.s3.amazonaws.com/wp-content/uploads/2020/09/brastemp_geladeira_jupiter_.mp4"
                 >
 

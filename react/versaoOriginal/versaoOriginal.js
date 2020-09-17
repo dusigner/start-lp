c4120c5a6d586d628e7c518a288ffb1f64807885
Eduardo Soares <eduardo@visie.com.br>
2020-09-17 14:10:36 -0300

    mudanca de nomenclaturas

---
+++ react/versaoOriginal/versaoOriginal.js
@@ -0,0 +1,55 @@
+import React, { Component } from 'react';
+import Headroom from 'react-headroom';
+
+// Layout
+import MenuEstatico from './components/menu-estatico/menu'
+import Menu from './components/menu/menu'
+import GeladeiraInverse from './layout/geladeira-inverse/geladeira-inverse';
+import Dispenser from './layout/dispenser/dispenser';
+import FreshControl from './layout/fresh-control/fresh-control';
+import ConvertibleSpace from './layout/convertible-space/convertible-space';
+import AtencaoDetalhes from './layout/atencao-detalhes/atencao-detalhes';
+import Video from '../versaoVideo/layout/video/video';
+import Vitrine from './layout/vitrine/vitrine'
+import VitrineCompra from './layout/vitrine-compra/vitrine'
+import EspecificacoesTecnicas from './layout/especificacoes/especificacoes';
+
+
+// Assets
+import './versaoOriginal.global.css';
+
+class versaoOriginal extends React.Component {
+
+	constructor(props) {
+		super(props);
+	}
+
+	componentDidMount() {
+
+	}
+
+	render() {
+
+		return (
+			<div className="lancamento__refri">
+
+				<Headroom disableInlineStyles>
+					{ this.props.versao == "qrcode" && <MenuEstatico/> }
+					{ this.props.versao == "video" && <Menu/> }
+				</Headroom>
+
+				<GeladeiraInverse versao={this.props.versao}></GeladeiraInverse>
+				<Dispenser></Dispenser>
+				<FreshControl></FreshControl>
+				<ConvertibleSpace></ConvertibleSpace>
+				<AtencaoDetalhes></AtencaoDetalhes>
+				<Video></Video>
+				{ this.props.versao == "qrcode" && <Vitrine/> }
+				{ this.props.versao == "video" && <VitrineCompra/> }
+				<EspecificacoesTecnicas></EspecificacoesTecnicas>
+
+			</div>
+		)
+	}
+}
+export default versaoOriginal;
